f4f2df6a721cbcea167892bfe8f0f4806788fc1f
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2018-12-11 20:58:07 -0500

    Second commit

--- public/main.js
+++ public/main.js
@@ -2,25 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var incrementButton = document.getElementById("Increment");
     var decrementButton = document.getElementById("Decrement");
     var counter = document.getElementById("counter");
-
-   var count = 0;
-
-    // incrementButton.addEventListener('click', function(){
-    //     incrementButton.innerHTML = "He said it again!"
-
-    //     count++;
-
-    //     counter.innerHTML = count;
     
-    // });
-    // decrementButton.addEventListener('click', function(){
-    //     decrementButton.innerHTML = "He said it again!"
-
-    //     count--;
-
-    //     counter.innerHTML = count;
-    
-    // });
     incrementButton.addEventListener('click', function(){
         axios.get('/api/increment').then(function(response){
             counter.innerHTML = response.data;
@@ -35,14 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-// axios
-//     .get('/api/count')
-//     .then(function(response){
-//         document.getElementById('count').innerHTML = response.data;
-// })
-// .catch(function(error){
-//     console.log(error);
-// })
+
 
 
 
